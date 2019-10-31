65bd9d7089438aa946b84cffb6cf3b2738fff229
aditep <topkoka4000@gmail.com>
2019-10-31 07:41:40 +0700

    update 30-10-2019v2  'update GUI Button clear data V2 '

--- RemoveJson.py
+++ RemoveJson.py
@@ -5,17 +5,15 @@ def removeJson():
     nameEx = 'cam'
     try:
         path = [os.path.join('dataSet/'+nameEx, f) for f in os.listdir('dataSet/'+nameEx)]
-        for id in range(len(path)+1):
-            file_path = ('dataSet/' + str(nameEx) + '/keypose.' + str(id) + ".json")
-            # print('remove file = '+file_path)
-            if os.path.exists(file_path):
-                os.remove(file_path)
+        for id in path:
+            if os.path.exists(id):
+                os.remove(id)
             else:
                 print("The file does not exist")
         print('removeJson Ok..')
     except IOError as e:
         print('removeJson :',e)
 
-
-if __name__ == '__main__':
-    print(removeJson())
+#
+# if __name__ == '__main__':
+#     print(removeJson())
